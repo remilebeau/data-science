7ddb57146465c6b222e837167180d0ac96fcd27c
remilebeau <remilebeau90@gmail.com>
2025-06-11 01:17:42 -0400

    change styling. add <MonteCarloExplanation />

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import MonteCarloExplanation from "@/components/MonteCarloExplanation";
 import ProductionForm from "@/components/ProductionForm";
 
 export default function Home() {
   return (
     <main>
+      <MonteCarloExplanation />
       <ProductionForm />
     </main>
   );
