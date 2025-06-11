97e5e02ec8047bdeab5213bba94d49856d51a0af
remilebeau <remilebeau90@gmail.com>
2025-06-11 01:03:00 -0400

    abstract production results into its own component

--- src/components/ProductionForm.tsx
+++ src/components/ProductionForm.tsx
@@ -18,6 +18,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import simulateProduction from "@/lib/simulateProduction";
 import { useState } from "react";
+import ProductionResults from "@/components/ProductionResults";
 
 const formSchema = z
   .object({
@@ -110,46 +111,7 @@ export default function ProductionForm() {
         </form>
       </Form>
       {simulationResults && (
-        <div className="mt-8">
-          <h2 className="text-2xl font-bold mb-4">Simulation Results</h2>
-          <p>
-            Expected Profit:{" "}
-            {simulationResults.expectedProfit.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
-            })}
-          </p>
-          <p>
-            Volatility:{" "}
-            {simulationResults.volatility.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
-            })}
-          </p>
-          <p>Sharpe Ratio: {simulationResults.sharpeRatio.toFixed(2)}</p>
-          <p>
-            Worst Likely Case:{" "}
-            {simulationResults.worstLikelyCase.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
-            })}
-          </p>
-          <p>
-            Best Likely Case:{" "}
-            {simulationResults.bestLikelyCase.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
-            })}
-          </p>
-        </div>
+        <ProductionResults simulationResults={simulationResults} />
       )}
     </>
   );
---
+++ src/components/ProductionResults.tsx
@@ -0,0 +1,48 @@
+type Props = {
+  simulationResults: SimulationResults;
+};
+
+export default function ProductionResults({ simulationResults }: Props) {
+  return (
+    <section className="mt-8">
+      <h2 className="text-2xl font-bold mb-4">Simulation Results</h2>
+      <p>
+        Expected Profit:{" "}
+        {simulationResults.expectedProfit.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        })}
+      </p>
+      <p>
+        Volatility:{" "}
+        {simulationResults.volatility.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        })}
+      </p>
+      <p>Sharpe Ratio: {simulationResults.sharpeRatio.toFixed(2)}</p>
+      <p>
+        Worst Likely Case:{" "}
+        {simulationResults.worstLikelyCase.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        })}
+      </p>
+      <p>
+        Best Likely Case:{" "}
+        {simulationResults.bestLikelyCase.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        })}
+      </p>
+    </section>
+  );
+}
