646a8c86f9b1ed4bff5812b4a613649e4228ed12
remilebeau <remilebeau90@gmail.com>
2025-06-12 03:28:06 -0400

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProductionForm from "@/components/ProductionForm";
 
 export default function Home() {
   return (
-    <main>
+    <main className="mx-auto mt-12 flex max-w-4xl flex-col gap-8 p-4">
       <ProductionExplanation />
       <ProductionForm />
     </main>
