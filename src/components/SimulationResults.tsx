5014ad22d95d4afb1bbd928a421408d5faffffc8
remilebeau <remilebeau90@gmail.com>
2025-08-01 15:38:23 -0400

    remove Inputs Table from SimulationResults.tsx

--- src/components/SimulationResults.tsx
+++ src/components/SimulationResults.tsx
@@ -11,6 +11,8 @@ type Props = {
   results: SimulationOutput;
 };
 
+const locale = "en-US";
+
 const currencyFormat: Intl.NumberFormatOptions = {
   style: "currency",
   currency: "USD",
@@ -25,87 +27,11 @@ const numberFormat: Intl.NumberFormatOptions = {
 export default function SimulationResults({ results }: Props) {
   return (
     <>
-      {/* INPUTS TABLE */}
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-1/2">Inputs</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {/* Production Quantity */}
-          <TableRow>
-            <TableCell>Production Quantity</TableCell>
-            <TableCell>
-              {results.productionQuantity.toLocaleString("en-US", numberFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Unit Cost */}
-          <TableRow>
-            <TableCell>Unit Cost</TableCell>
-            <TableCell>
-              {results.unitCost.toLocaleString("en-US", currencyFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Unit Price */}
-          <TableRow>
-            <TableCell>Unit Price</TableCell>
-            <TableCell>
-              {results.unitPrice.toLocaleString("en-US", currencyFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Salvage Price */}
-          <TableRow>
-            <TableCell>Salvage Price</TableCell>
-            <TableCell>
-              {results.salvagePrice.toLocaleString("en-US", currencyFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Fixed Cost */}
-          <TableRow>
-            <TableCell>Fixed Cost</TableCell>
-            <TableCell>
-              {results.fixedCost.toLocaleString("en-US", currencyFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Worst Likely Demand */}
-          <TableRow>
-            <TableCell>Worst Likely Demand</TableCell>
-            <TableCell>
-              {results.worstLikelyDemand.toLocaleString("en-US", numberFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Expected Demand */}
-          <TableRow>
-            <TableCell>Expected Demand</TableCell>
-            <TableCell>
-              {results.expectedDemand.toLocaleString("en-US", numberFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Best Likely Demand */}
-          <TableRow>
-            <TableCell>Best Likely Demand</TableCell>
-            <TableCell>
-              {results.bestLikelyDemand.toLocaleString("en-US", numberFormat)}
-            </TableCell>
-          </TableRow>
-          {/* Demand Standard Deviation */}
-          <TableRow>
-            <TableCell>Demand Standard Deviation</TableCell>
-            <TableCell>
-              {results.demandStandardDeviation.toLocaleString(
-                "en-US",
-                numberFormat,
-              )}
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
       {/* SIMULATION RESULTS TABLE */}
-      <Table>
+      <Table className="mx-auto w-1/2">
         <TableHeader>
           <TableRow>
-            <TableHead className="w-1/2">Results</TableHead>
+            <TableHead>Results</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -113,35 +39,35 @@ export default function SimulationResults({ results }: Props) {
           <TableRow>
             <TableCell>Expected Profit</TableCell>
             <TableCell>
-              {results.expectedProfit.toLocaleString("en-US", currencyFormat)}
+              {results.expectedProfit.toLocaleString(locale, currencyFormat)}
             </TableCell>
           </TableRow>
           {/* Volatility */}
           <TableRow>
             <TableCell>Volatility</TableCell>
             <TableCell>
-              {results.volatility.toLocaleString("en-US", currencyFormat)}
+              {results.volatility.toLocaleString(locale, currencyFormat)}
             </TableCell>
           </TableRow>
           {/* Sharpe Ratio */}
           <TableRow>
             <TableCell>Sharpe Ratio</TableCell>
             <TableCell>
-              {results.sharpeRatio.toLocaleString("en-US", numberFormat)}
+              {results.sharpeRatio.toLocaleString(locale, numberFormat)}
             </TableCell>
           </TableRow>
           {/* Worst Likely Case */}
           <TableRow>
             <TableCell>Worst Likely Case</TableCell>
             <TableCell>
-              {results.worstLikelyCase.toLocaleString("en-US", currencyFormat)}
+              {results.worstLikelyCase.toLocaleString(locale, currencyFormat)}
             </TableCell>
           </TableRow>
           {/* Best Likely Case */}
           <TableRow>
             <TableCell>Best Likely Case</TableCell>
             <TableCell>
-              {results.bestLikelyCase.toLocaleString("en-US", currencyFormat)}
+              {results.bestLikelyCase.toLocaleString(locale, currencyFormat)}
             </TableCell>
           </TableRow>
         </TableBody>
